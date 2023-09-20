9c3432b98e877d206a78f06b5a17d6c786d42de0
EsmeraldaMar <6580@holbertonstudents.com>
2023-09-20 10:44:10 +1000

    fixed requirement

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,6 +1,2 @@
 #!/usr/bin/python3
-# This is a python script that prints a message
-
-#Using the print function to print the message
-print ("Programming is like building a multilingual puzzle, ")
-print() #prints a new line
+print ("\"Programming is like building a multilingual puzzle,")
