dbf7efa275682758fe806408b8a81ba178865376
EsmeraldaMar <6580@holbertonstudents.com>
2023-10-02 23:00:30 +1100

    corrected code

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) < len(tuple_b):
+        tuple_a, tuple_b = tuple_b, tuple_a
     res = []
     for i in range(0, len(tuple_a)):
         if (i < len(tuple_b)):
