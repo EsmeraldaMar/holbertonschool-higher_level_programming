2df035bbc2e0daca478ed83504095bd2ab2dca41
EsmeraldaMar <6580@holbertonstudents.com>
2023-10-02 23:17:28 +1100

    fixed code

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < len(tuple_b):
-        tuple_a, tuple_b = tuple_b, tuple_a
+    while len(tuple_a) < 2:
+        tuple_a += (0,)
+    while len(tuple_b) < 2:
+        tuple_b += (0,)
     res = []
-    for i in range(0, len(tuple_a)):
-        if (i < len(tuple_b)):
-            res.append(tuple_a[i] + tuple_b[i])
-        else:
-            res.append(tuple_a[i])
+    for i in range(0, 2):
+        res.append(tuple_a[i] + tuple_b[i])
     res = tuple(res)
     return (res)
