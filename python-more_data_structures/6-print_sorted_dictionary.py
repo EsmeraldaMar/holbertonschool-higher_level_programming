b0cf98debd0628447cbb4d77acba44832d34f8d7
EsmeraldaMar <6580@holbertonstudents.com>
2023-10-03 23:03:53 +1100

    fixed format

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,6 +2,5 @@
 def print_sorted_dictionary(a_dictionary):
     my_keys = list(a_dictionary.keys())
     my_keys.sort()
-    sorted_dic = {i: a_dictionary[i] for i in my_keys}
     for key in my_keys:
-        print(key, sorted_dic[key])
+        print("{}: {}".format(key, a_dictionary[key]))
