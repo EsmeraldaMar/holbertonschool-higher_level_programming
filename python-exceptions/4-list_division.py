29ff1564b736edbf4ad4f28cd75deaf5cfd76602
EsmeraldaMar <6580@holbertonstudents.com>
2023-10-06 00:23:28 +1100

    fixed code

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -11,5 +11,5 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             print("divison by 0")
         finally:
-            continue
+            pass
     return new_list
