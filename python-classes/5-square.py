04b0ef30b8e3a54aaaa5294ddd010d48551dc732
EsmeraldaMar <6580@holbertonstudents.com>
2023-10-09 16:24:40 +1100

    added task 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""It defines class square"""
+
+
+class Square:
+    """
+    Class that defines properties of square based on 1-square
+    """
+    def __init__(self, size=0):
+        """Initialise new square
+            Args:
+                size(int): size of new square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.size):
+                print('#' * self.size)
