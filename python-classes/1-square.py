0c45279645d3b401d5bf8aa4f3691012013487a2
EsmeraldaMar <6580@holbertonstudents.com>
2023-10-09 13:55:41 +1100

    fixed spacing

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """It defines a class Square"""
 
+
 class Square:
     """
     Class that defines properties of the square based on 0-square.py
     """
-    def __init__(self,size):
+    def __init__(self, size):
         """ Initialize new square
 
             Args:
