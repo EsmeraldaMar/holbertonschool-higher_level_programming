65cf1ab361f67670198aac91d950eac784c6d190
EsmeraldaMar <6580@holbertonstudents.com>
2023-10-09 14:49:09 +1100

    fixed space

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -16,9 +16,11 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -26,6 +28,6 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-   
+
     def area(self):
         return (self.__size * self.__size)
