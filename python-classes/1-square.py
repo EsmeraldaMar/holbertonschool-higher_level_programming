952ff6d9463360768ea89bec621e33fc946255c8
EsmeraldaMar <6580@holbertonstudents.com>
2023-10-09 13:54:28 +1100

    added task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""It defines a class Square"""
+
+class Square:
+    """
+    Class that defines properties of the square based on 0-square.py
+    """
+    def __init__(self,size):
+        """ Initialize new square
+
+            Args:
+            size (int): size of new square
+        """
+        self._size = size
