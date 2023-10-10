2f4adee9c6530dd0fa835b1dda7fbc17c9f2004f
EsmeraldaMar <6580@holbertonstudents.com>
2023-10-10 22:20:51 +1100

    fixed function to shorten

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -15,8 +15,8 @@ def add_integer(a, b=98):
     b (int, optional): second value defaults to 98
     returns the sum
     """
-    if a is None or (isinstance(a, int) is not True and isinstance(a, float) is not True):
+    if a is None or (type(a) is not int and type(a) is not float):
         raise TypeError("a must be an integer")
-    if b is None or (isinstance(b, int) is not True and isinstance(b, float) is not True):
+    if a is None or (type(b) is not int and type(b) is not float):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
