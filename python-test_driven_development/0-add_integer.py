314e34391b38e9ae048b87beb305227ee9a602ed
EsmeraldaMar <6580@holbertonstudents.com>
2023-10-10 20:31:35 +1100

    fixed test file and code

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
 """
-This module contains one function for adding 2 integers together
+Module Name: 0-add_integer.py
+des: defines as integer addition function
+function name: add_integer()
 """
 
 
 def add_integer(a, b=98):
     """
-    Sums up the arguments passed in.
+    add_integer: adds integers and floats
+
     Args:
-        a (int) : first one
-        b (int) : second one (default value 98)
-    Returns:
-        int: sum of a and b
+    a(int): First value
+    b (int, optional): second value defaults to 98
+    returns the sum
     """
-    if (isinstance(a, int) is not True and isinstance(a, float) is not True):
+    if a is None or (isinstance(a, int) is not True and isinstance(a, float) is not True):
         raise TypeError("a must be an integer")
-    if (isinstance(b, int) is not True and isinstance(b, float) is not True):
+    if b is None or (isinstance(b, int) is not True and isinstance(b, float) is not True):
         raise TypeError("b must be an integer")
     #if (a == float('NaN')):
         #raise ValueError("can't convert NaN to integer")
@@ -23,5 +25,4 @@ def add_integer(a, b=98):
     #     a = int(a)
     # if isinstance(b, float):
     #     b = int(b)
-    result = int(a) + int(b)
-    return result
+    return int(a) + int(b)
