a4086bcbaf1f791cf8d2cf71cbcb4993566541cc
EsmeraldaMar <6580@holbertonstudents.com>
2023-10-10 22:28:24 +1100

    fixed indentation

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -13,10 +13,14 @@ def add_integer(a, b=98):
     Args:
     a(int): First value
     b (int, optional): second value defaults to 98
-    returns the sum
+
+    Returns:
+     int: the sum of a and b
     """
     if a is None or (type(a) is not int and type(a) is not float):
         raise TypeError("a must be an integer")
-    if a is None or (type(b) is not int and type(b) is not float):
+
+    if b is None or (type(b) is not int and type(b) is not float):
         raise TypeError("b must be an integer")
+
     return int(a) + int(b)
