c3f151576f626d9498e44377766566bb18f0ebd9
EsmeraldaMar <6580@holbertonstudents.com>
2023-10-12 15:31:28 +1100

    added task 7

---
+++ python-more_classes/7-rectangle.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""
+New class for Rectangle
+"""
+
+
+class Rectangle:
+    """ Rectangle Class based on 6-rectangle """
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    """ Width """
+    @property
+    def width(self):
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    """ Height """
+    @property
+    def height(self):
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+    """ Area of Rectangle """
+    def area(self):
+        return self.width * self.height
+
+    """ Perimeter of Rectangle"""
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
+
+    """ Print Rectangle using # """
+    def __str__(self):
+        empty_string = ""
+        if self.__width == 0 or self.__height == 0:
+            return (empty_string)
+        symbol_str = str(self.print_symbol)
+        return (((symbol_str * self.width) + "\n") * self.height)[:-1]
+
+    """ Returns 'official' string rep of an instance"""
+    def __repr__(self):
+        return ("Rectangle({:d}, {:d})".format(self.__width, self.__height))
+
+    """ Print a message when the instance is deleted """
+    def __del__(self):
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
