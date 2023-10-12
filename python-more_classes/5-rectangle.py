206e89016b0a3bf22d1788a8f56cb5fb14dc324a
EsmeraldaMar <6580@holbertonstudents.com>
2023-10-12 14:25:36 +1100

    added task 5

---
+++ python-more_classes/5-rectangle.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""
+New class for Rectangle
+"""
+
+
+class Rectangle:
+    """ Rectangle Class based on 4-rectangle """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+    """ Width """
+    @property
+    def width(self):
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    """ Height """
+    @property
+    def height(self):
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+    """ Area of Rectangle """
+    def area(self):
+        return self.width * self.height
+
+    """ Perimeter of Rectangle"""
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
+
+    """ Print Rectangle using # """
+    def __str__(self):
+        empty_string = ""
+        if self.__width == 0 or self.__height == 0:
+            return (empty_string)
+
+        return ((('#' * self.width) + "\n") * self.height)[:-1]
+
+    """ Returns 'official' string rep of an instance"""
+    def __repr__(self):
+        return ("Rectangle({:d}, {:d})".format(self.__width, self.__height))
+
+    """ Print a message when the instance is about to be deleted """
+    def __del__(self):
+        print("Bye rectangle...")
