1ef9c6c7ca32ac5064471ad20cf4acc255efe42e
EsmeraldaMar <6580@holbertonstudents.com>
2023-10-12 11:46:55 +1100

    added task 1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+New class for Rectangle
+"""
+
+
+class Rectangle:
+    """ Rectangle Class based on 0-rectangle """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+    """ Width """
+    @property
+    def width(self):
+        return (self.__width)
+  
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+       
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+    
+    """ Height """
+    @property
+    def height(self):
+        resturn (self.__height)
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__height = value
