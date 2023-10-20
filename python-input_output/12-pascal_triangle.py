775dc58944ab2bc37d2753043d1000a0afa126ad
EsmeraldaMar <6580@holbertonstudents.com>
2023-10-21 00:31:02 +1100

    pascal triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Pascal Triangle"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    final = []
+    for i in range(1, n + 1):
+        new_list = []
+        if i < 3:
+            for _ in range(i):
+                new_list.append(1)
+            final.append(new_list)
+        else:
+            new_list.append(1)
+            for j in range(len(final[i - 2])):
+                if (j + 1 < len(final[i - 2])):
+                    new_list.append(final[i - 2][j] + final[i - 2][j + 1])
+            new_list.append(1)
+            final.append(new_list)
+    return final
