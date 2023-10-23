f4b6097a24a872f6a1ade7b04de77d25aa378da4
EsmeraldaMar <6580@holbertonstudents.com>
2023-10-23 23:20:51 +1100

    Task 7 - Better display

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -72,8 +72,12 @@ class Rectangle(Base):
 
     def display(self):
         """ Prints in stdout the Rectangle instance with the character #"""
-        for i in range(self.__height):
-            for j in range(self.__width):
+        for _ in range(self.__y):
+            print()
+        for _ in range(self.__height):
+            for _ in range(self.__x):
+                    print(" ", end="")
+            for _ in range(self.__width):
                 print("#", end="")
             print()
 
