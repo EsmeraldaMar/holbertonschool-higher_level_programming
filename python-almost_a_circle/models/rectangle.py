e79ce715e9a401d1baca5b677f3377e766ea4f6f
EsmeraldaMar <6580@holbertonstudents.com>
2023-10-23 14:37:39 +1100

    comment

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-from models.base import Base
 """ New Rectangle Class """
+from models.base import Base
 
 
 class Rectangle(Base):
     """ Rectangle class that inherits from base"""
-
     def __init__(self, width, height, x=0, y=0, id=None):
         """ Initialize the rectangle instance"""
         super().__init__(id)
