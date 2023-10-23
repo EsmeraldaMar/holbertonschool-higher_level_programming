b0fa09950765f4a935d749f85546ac39d69c39e7
EsmeraldaMar <6580@holbertonstudents.com>
2023-10-23 14:21:24 +1100

    added sets and gets

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+from .base import Base
+""" New Rectangle Class """
+
+
+class Rectangle(Base):
+    """ Rectangle class that inherits from base"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ Initialize the rectangle instance"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    """ Width Getter and Setter"""
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        self.__width = width
+
+    """ Height Getter and Setter"""
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        self.__height = height
+
+    """ x Getter and Setter"""
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        self.__x = x
+
+    """ y Getter and Setter"""
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        self.__y = y
