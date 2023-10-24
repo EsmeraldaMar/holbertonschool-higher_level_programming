f1fefdfbed308ea1d67d64b1be71ad3685ac1f92
EsmeraldaMar <6580@holbertonstudents.com>
2023-10-24 15:17:14 +1100

    created square

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" New class square"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ Square class that inherits from Rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Class Constructor"""
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    def __str__(self):
+        """ Returns [Square] (<id>) <x>/<y> - <width>/<height>"""
+        return ("[{}] ({}) {:d}/{:d} - {:d}".format(
+            self.__class__.__name__, self.id, self.x,
+            self.y, self.size))
