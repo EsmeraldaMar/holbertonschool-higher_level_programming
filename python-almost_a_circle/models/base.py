21c718b002d6b5baf5b9e2741ad7d38e1a4db457
EsmeraldaMar <6580@holbertonstudents.com>
2023-10-24 17:02:03 +1100

    added json str rep for list

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Create a new class Base """
-
+from os import path
+import json
 
 class Base:
     """ New Class Base"""
@@ -12,3 +13,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of list_dictionaries."""
+        new = []
+        if list_dictionaries is None:
+            return new
+        else:
+            return (json.dumps(list_dictionaries))
