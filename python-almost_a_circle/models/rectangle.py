0be2bb25b8ed589f07584ef601dfd472b49c0c47
EsmeraldaMar <6580@holbertonstudents.com>
2023-10-24 14:43:46 +1100

    kwargs

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -87,7 +87,7 @@ class Rectangle(Base):
             self.__class__.__name__, self.id, self.__x,
             self.__y, self.__width, self.__height))
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """ Assigns an argument to each attribute"""
         try:
             self.id = args[0]
