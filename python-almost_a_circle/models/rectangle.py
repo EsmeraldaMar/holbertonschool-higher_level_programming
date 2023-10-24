2ef1da8190b1f1688626fd9d247010a548cafcef
EsmeraldaMar <6580@holbertonstudents.com>
2023-10-24 15:57:36 +1100

    added dictionary on rec

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -101,6 +101,16 @@ class Rectangle(Base):
 
     def __str__(self):
         """ Returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
-        return ("[{}] ({}) {:d}/{:d} - {:d}/{:d}".format(
+        return ("[Rectangle] ({}) {:d}/{:d} - {:d}/{:d}".format(
             self.__class__.__name__, self.id, self.__x,
             self.__y, self.__width, self.__height))
+
+    def to_dictionary(self):
+        """ Returns the dictionary representation of a Rectangle"""
+        return {
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
+            }
