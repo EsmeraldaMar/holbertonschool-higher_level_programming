53a043bce1ff84021ac5e4029dabff155eaec23d
EsmeraldaMar <6580@holbertonstudents.com>
2023-11-27 14:47:06 +1100

    feat: script prote ted against mysql injections

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Script that takes in an argument and displays all (mySQL inj)
+values in the states table of hbtn_0e_0_usa where name matches the argument.
+
+"""
+import MySQLdb
+import sys
+
+# Code should not be executed when imported
+if __name__ == "__main__":
+
+    # Makes a connection to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+
+    # Gives us the ability to have multiple separate working environments
+    # through the same connection to the database
+    cursor = db.cursor()
+    search_name = sys.argv[4]
+    cursor.execute("SELECT * FROM states WHERE BINARY name\
+                    LIKE %s ORDER BY id;".format(search_name))
+
+    rows = cursor.fetchall()
+    for i in rows:
+        print(i)
+    # Clean up Process
+    cursor.close()
+    db.close()
