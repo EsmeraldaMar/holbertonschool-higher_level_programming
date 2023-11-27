2cea7162c88792b821b67b3174010c411dfa1393
EsmeraldaMar <6580@holbertonstudents.com>
2023-11-27 21:34:10 +1100

    fix: removed comments

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,37 +1,24 @@
 #!/usr/bin/python3
-"""
-Script that prints the first State object from the database hbtn_0e_6_usa
-"""
+"""Prints the first State object from the database hbtn_0e_6_usa"""
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 import sys
 
-# Code should not be executed when imported
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-# The database connection details are formatted from the command line arguments
-# Create all tables in the database which are defined
-# by Base's subclasses such as State
     Base.metadata.create_all(engine)
 
-    # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
-    # Bind the session factory to the database engine
     Session.configure(bind=engine)
 
-    # Create a Session instance to handle transactions
     session = Session()
 
-    # Query the State table and order by the id of the state
     result = session.query(State).order_by(State.id).first()
 
-    # Iterate through each row in the query
-    # result and print the state id and name
     if result:
-        print("{}: {}".format(instance.id, instance.name))
+        print("{}: {}".format(result.id, result.name))
     else:
         print("Nothing")
-    # Close the session to release the connection
     session.close()
