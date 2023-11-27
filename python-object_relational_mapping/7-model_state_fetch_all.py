7f28a28c4be6d78a8f8fc745fb82cd432bbc8ec0
EsmeraldaMar <6580@holbertonstudents.com>
2023-11-27 21:16:23 +1100

    fix: comments

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,7 +12,8 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 # The database connection details are formatted from the command line arguments
-# Create all tables in the database which are defined by Base's subclasses such as State
+# Create all tables in the database which are defined
+# by Base's subclasses such as State
     Base.metadata.create_all(engine)
 
     # Create a configured "Session" class
@@ -24,7 +25,8 @@ if __name__ == "__main__":
     # Query the State table and order by the id of the state
     result = session.query(State).order_by(State.id)
 
-    # Iterate through each row in the query result and print the state id and name
+    # Iterate through each row in the query
+    # result and print the state id and name
     for instance in result:
         print("{}: {}".format(instance.id, instance.name))
 
