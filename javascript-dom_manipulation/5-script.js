a695b60705030e8db67f98dece0ab067c3935dac
EsmeraldaMar <6580@holbertonstudents.com>
2024-02-01 13:58:22 +1100

    added task 5

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,9 @@
+const updateHeader = document.getElementById('update_header');
+
+updateHeader.addEventListener('click', function() {
+
+  const updateheader = document.querySelector('header');
+
+  updateheader.textContent = 'New Header!!!';
+
+});
